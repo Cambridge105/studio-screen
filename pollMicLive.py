8959ee6f001ecaa6999e966d039badc590e415ca
Robert Chipperfield <robert@syxis.co.uk>
2015-01-24 13:51:11 +0000

    Try doing web app instead

--- pollMicLive.py
+++ pollMicLive.py
@@ -2,15 +2,20 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
  
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-from os import curdir, sep
+import web
 
-PORT_NUMBER = 8080
+PORT_NUMBER = 8081
 
-class myHandler(BaseHTTPRequestHandler):
+urls = (
+    '/miclive', 'miclive'
+)
+
+app = web.application(urls, globals())
+
+class miclive(self):
 	
 	#Handler for the GET requests
-	def do_GET(self):
+	def GET(self):
 		GPIO.setup("P9_11", GPIO.IN) #AKA GPIO_30 see diagram at http://kilobaser.com/blog/2014-07-15-beaglebone-black-gpios
 
 		newstate = GPIO.input("P9_11")
@@ -19,24 +24,12 @@ class myHandler(BaseHTTPRequestHandler):
 		# f.write(json)
 		# f.close
 				
-		f = open(curdir + sep + self.path) 
 		self.send_response(200)
 		self.send_header('Content-type',"application/json")
 		self.end_headers()
 		self.wfile.write(json)
-		f.close()
 
 		GPIO.cleanup()
 
-try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
-	
-	#Wait forever for incoming htto requests
-	server.serve_forever()
-
-except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
-	server.socket.close()
+if _name_ == "_main_":
+    app.run("0.0.0.0:8081")
