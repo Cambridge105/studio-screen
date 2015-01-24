a09e48f62d2db57f924f442ba08ce31605fcdd42
dnas2 <dom@domsmith.co.uk>
2015-01-24 11:47:20 +0000

    Silly python indents...

--- pollMicLive.py
+++ pollMicLive.py
@@ -5,13 +5,13 @@ import time
 GPIO.setup("P9_11", GPIO.IN) #AKA GPIO_30 see diagram at http://kilobaser.com/blog/2014-07-15-beaglebone-black-gpios
  
 while True:
-        GPIO.wait_for_edge("P9_11")
-		time.sleep(0.1)
-		newstate = GPIO.input("P9_11")
-		json = '{ "micLiveState": ' + str(newstate) + '}'
-		f = open('micLive.js','w')
-		f.write(json)
-		f.close
+	GPIO.wait_for_edge("P9_11",GPIO.BOTH)
+	time.sleep(0.1)
+	newstate = GPIO.input("P9_11")
+	json = '{ "micLiveState": ' + str(newstate) + '}'
+	f = open('micLive.js','w')
+	f.write(json)
+	f.close
 			
 GPIO.cleanup()
   
