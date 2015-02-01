e620528af5538de7c54abc60ba95baee41bac428
dnas2 <dom@domsmith.co.uk>
2015-02-01 20:40:48 +0000

    Sunday Breakfast timings

--- messages/sunday.js
+++ messages/sunday.js
@@ -1,13 +1,16 @@
 [
 	{"h":8,"m":19,"msg":"TRAVEL","d":3},
-	{"h":8,"m":29,"msg":"HEADLINES","d":2},
+	{"h":8,"m":29,"msg":"WHAT'S ONS","d":2},
 	{"h":8,"m":39,"msg":"TRAVEL","d":3},
+	{"h":8,"m":45,"msg":"NEWSPAPER FRONT PAGES","d":2},
 	{"h":8,"m":54,"msg":"TRAVEL","d":2},
+	{"h":9,"m":09,"msg":"CAMBRIDGE QUIZ","d":2},
 	{"h":9,"m":19,"msg":"TRAVEL","d":3},
-	{"h":9,"m":29,"msg":"HEADLINES","d":2},
-	{"h":9,"m":39,"msg":"TRAVEL","d":3},
-	{"h":10,"m":19,"msg":"TRAVEL","d":3},
-	{"h":10,"m":39,"msg":"TRAVEL","d":3},
+	{"h":9,"m":29,"msg":"COMMUNITY DESK","d":2},
+	{"h":9,"m":34,"msg":"SUNDAY PAPER REVIEW","d":2},
+	{"h":9,"m":49,"msg":"105 SPORT REVIEW","d":2},
+	{"h":10,"m":09,"msg":"ENTERTAINMENT NEWS","d":2},
+	{"h":10,"m":29,"msg":"GUEST INTERVIEW","d":2},
 	{"h":11,"m":19,"msg":"TRAVEL","d":3},
 	{"h":11,"m":39,"msg":"TRAVEL","d":3}
 ]
