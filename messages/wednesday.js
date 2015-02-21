cc6e8dafa2985303079e402821dc02e3904f429c
Dominic Smith <dom@domsmith.co.uk>
2015-02-21 00:29:52 +0000

    No traffic for TGTBF

--- messages/wednesday.js
+++ messages/wednesday.js
@@ -7,8 +7,6 @@
 	{"h":8,"m":29,"msg":"HEADLINES","d":2},
 	{"h":8,"m":39,"msg":"TRAVEL","d":3},
 	{"h":8,"m":54,"msg":"TRAVEL","d":2},
-	{"h":9,"m":19,"msg":"TRAVEL","d":3},
-	{"h":9,"m":39,"msg":"TRAVEL","d":3},
 	{"h":10,"m":19,"msg":"TRAVEL","d":3},
 	{"h":10,"m":22,"msg":"FIRE ALARM TEST DUE SOON","d":8,"c":"coral"},
 	{"h":10,"m":39,"msg":"TRAVEL","d":3},
