4c05b96985ef82697d2380478b35473a606a3f95
Robert Chipperfield <robert.chipperfield@red-gate.com>
2016-01-04 14:50:09 +0000

    Include travel in 105 Today

--- messages/friday.js
+++ messages/friday.js
@@ -5,6 +5,8 @@
 	{"h":8,"m":19,"msg":"TRAVEL","d":3},
 	{"h":8,"m":29,"msg":"HEADLINES","d":2},
 	{"h":8,"m":39,"msg":"TRAVEL","d":3},
+	{"h":9,"m":19,"msg":"TRAVEL","d":3},
+	{"h":9,"m":39,"msg":"TRAVEL","d":3},
 	{"h":10,"m":19,"msg":"TRAVEL","d":3},
 	{"h":10,"m":39,"msg":"TRAVEL","d":3},
 	{"h":11,"m":19,"msg":"TRAVEL","d":3},
