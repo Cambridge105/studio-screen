bfe3c67abfe2680c0ad48288a6ea91a340337c82
Robert Chipperfield <robert@syxis.co.uk>
2022-12-06 23:00:07 +0000

    Typo

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function loadSlides() {
 	if (loadedFromGreenroom)
 	{
 		slideTxt = "<div id='img0' class='slideimg'><img src='slides/welcome.jpg'></div>";
-		slideTxt += "<div id='img1' class='slideimg'><img src='slides/accrossthecity.jpg'></div>";
+		slideTxt += "<div id='img1' class='slideimg'><img src='slides/acrossthecity.jpg'></div>";
 		slideTxt += "<div id='img2' class='slideimg'><img src='slides/awards2022.jpg'></div>";
 		slideTxt += "<div id='img3' class='slideimg'><img src='slides/liveandlocal.jpg'></div>";
 		slideTxt += "<div id='img4' class='slideimg'><img src='slides/localnews.jpg'></div>";
